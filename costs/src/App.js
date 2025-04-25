3b7d44b14a2c431031378d62bdcfa945af43013b
gustavo <gustavodaniel141516@gmail.com>
2025-04-25 12:45:41 -0300

    Atualizações

--- costs/src/App.js
+++ costs/src/App.js
@@ -3,23 +3,23 @@ import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
 
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
-
+      <Navbar />
       <Switch>
         <Container customClass="min-height">
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route path="//projects">
+            <Projects />
           </Route>
           <Route path="/company">
             <Company />
@@ -33,7 +33,7 @@ function App() {
         </Container>
       </Switch>
 
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
